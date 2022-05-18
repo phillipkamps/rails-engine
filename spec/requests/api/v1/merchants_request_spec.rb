8a4692cf0c4f16b7751cb7c3a3a3c1acaa0ce5e1
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-18 12:10:56 -0600

    Test: response for one merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,16 +1,36 @@
 require "rails_helper"
 
 RSpec.describe "Merchants API" do
-  it "sends a list of all merchants" do
+  it "gets all merchants" do
     create_list(:merchant, 3)
 
     get "/api/v1/merchants"
-    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    merchants = parsed[:data]
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants[:data].count).to eq(3)
-    expect(merchants[:data][0][:id]).to be_a(String)
-    expect(merchants[:data][1][:type]).to eq("merchant")
-    expect(merchants[:data][2][:attributes][:name]).to be_a(String)
+    expect(response).to be_successful
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq("merchant")
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it "gets one merchant" do
+    merchant_list = create_list(:merchant, 3)
+
+    get "/api/v1/merchants/#{merchant_list.first.id}"
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    merchants = parsed[:data]
+
+    expect(response).to be_successful
+    expect(merchants.count).to eq(3)
   end
 end
