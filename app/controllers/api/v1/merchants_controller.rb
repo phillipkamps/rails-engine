6caadccac8ca57e8019eaf37727c3fccfc4f5a29
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-17 18:29:46 -0600

    Reflect serializer gem syntax

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    # merchant = Merchant.all
-    # render json: BookSerializer.format_books(books)
+    render json: MerchantsSerializer.new(Merchant.all)
   end
 end
