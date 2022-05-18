51665636dade908aab29bf74f9f17116cd1706e9
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-17 18:28:55 -0600

    Begin to set up merchant index endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.describe "Merchants API" do
   it "sends a list of all merchants" do
     get "/api/v1/merchants"
-
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(100)
   end
 end
