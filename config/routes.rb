895f941137994445153efb209154623626517f25
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-19 20:13:48 -0600

    Fix: pluralization mistake

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
+
       resources :items do
-        resources :merchants, only: [:index], controller: :item_merchants
+        resources :merchant, only: [:index], controller: :item_merchants
       end
     end
   end
